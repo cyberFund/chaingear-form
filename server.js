854ce8cf8d9851c1473ad791cacac2b50ade82d6
Maxim Odegov <cvlt.mao@gmail.com>
2018-03-07 15:50:45 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+app.use(express.static('views/dist'))
+app.use('/static', express.static('files'))
+
+const port = 8080
+
+app.listen(port, () => {
+  console.log('listen 8000')
+})
+
+
+
